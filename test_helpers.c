4ee5119905a80640c4c9b34ae97c9f68315cf353
Felix Fung <f2fung@uwaterloo.ca>
2013-05-21 12:01:55 -0400

    Replace hardtabs in test_helpers with 2 spaces

--- test_helpers.c
+++ test_helpers.c
@@ -9,9 +9,9 @@ static int failure;
  */
 
 void print_message(char* message) {
-	if (message) {
-		bwprintf(COM2, "%s : ", message);
-	}
+  if (message) {
+    bwprintf(COM2, "%s : ", message);
+  }
 }
 
 /* Public Methods */
@@ -29,55 +29,55 @@ int did_fail() {
 }
 
 void assert_equals(char a, char b, char* message) {
-	if (a != b) {
-		print_message(message);
-		bwprintf(COM2, "Expected (%c, %x) but got (%c, %x) \n", a, a, b, b);
+  if (a != b) {
+    print_message(message);
+    bwprintf(COM2, "Expected (%c, %x) but got (%c, %x) \n", a, a, b, b);
     record_failure();
-		Exit();
-	}
+    Exit();
+  }
 }
 
 void assert_int_equals(int a, int b, char* message) {
-	if (a != b) {
-		print_message(message);
-		bwprintf(COM2, "Expected %d but got %d \n", a, b);
+  if (a != b) {
+    print_message(message);
+    bwprintf(COM2, "Expected %d but got %d \n", a, b);
     record_failure();
-		Exit();
-	}
+    Exit();
+  }
 }
 
 void assert_no_error(char* error, char* message) {
-	if (error) {
-		print_message(message);
-		bwprintf(COM2, "Unexpected error: %s\n", error);
+  if (error) {
+    print_message(message);
+    bwprintf(COM2, "Unexpected error: %s\n", error);
     record_failure();
-		Exit();
-	}
+    Exit();
+  }
 }
 
 void assert_error(char* error, char* message) {
-	if (!error) {
-		print_message(message);
-		bwprintf(COM2, "Expected error but got null\n");
+  if (!error) {
+    print_message(message);
+    bwprintf(COM2, "Expected error but got null\n");
     record_failure();
-		Exit();
-	}
+    Exit();
+  }
 }
 
 void assert_true(int boolean, char* message) {
-	if (!boolean) {
-		print_message(message);
-		bwprintf(COM2, "Expression not true\n");
+  if (!boolean) {
+    print_message(message);
+    bwprintf(COM2, "Expression not true\n");
     record_failure();
-		Exit();
-	}
+    Exit();
+  }
 }
 
 void assert_false(int boolean, char* message) {
-	if (boolean) {
-		print_message(message);
-		bwprintf(COM2, "Expression not false\n");
+  if (boolean) {
+    print_message(message);
+    bwprintf(COM2, "Expression not false\n");
     record_failure();
-		Exit();
-	}
+    Exit();
+  }
 }
