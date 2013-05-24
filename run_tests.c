d1df41b07fc88231e37f4a6c57d20cb5bcab8946
Felix Fung <f2fung@uwaterloo.ca>
2013-05-24 15:10:50 -0400

    Freezing Branch A1 for Submission

--- run_tests.c
+++ run_tests.c
@@ -4,10 +4,10 @@
 void run_tests() {
   // This MUST run first because of assumptions about task ids.
   // TODO(dzelemba): Fix this.
-  run_basic_test();
-  run_multiple_priorities_test();
-  run_context_switch_speed_test();
-  run_task_creation_errors_test();
+  /*run_basic_test();*/
+  /*run_multiple_priorities_test();*/
+  /*run_context_switch_speed_test();*/
+  /*run_task_creation_errors_test();*/
 
   run_assignment_1_test();
 }
